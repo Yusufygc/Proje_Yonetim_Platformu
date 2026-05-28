669a6043e98f394e0ed9320a5125b36e8d14aec3
Yusufygc <yusufygc118@gmail.com>
2026-05-28 18:51:19 +0300

    refactor(domain): model modülü başlatıcısı güncellendi
    Uygulamadaki tüm veri modellerinin dışa aktarımını sağlayan init dosyası düzenlendi.

---
+++ domain/models/__init__.py
@@ -0,0 +1,37 @@
+"""ORM model imports.
+
+Importing this package registers every model with SQLAlchemy metadata before
+table creation or migrations run.
+"""
+
+from domain.models.activity_log import ActivityLog
+from domain.models.attachment import Attachment
+from domain.models.checklist_item import ChecklistItem
+from domain.models.decision_record import DecisionRecord
+from domain.models.idea import Idea
+from domain.models.note import Note
+from domain.models.project import Project
+from domain.models.project_idea import ProjectIdea
+from domain.models.project_stage import ProjectStage
+from domain.models.project_tag import ProjectTag
+from domain.models.resource import Resource
+from domain.models.setting import Setting
+from domain.models.task import Task
+from domain.models.workflow_stage import WorkflowStage
+
+__all__ = [
+    "ActivityLog",
+    "Attachment",
+    "ChecklistItem",
+    "DecisionRecord",
+    "Idea",
+    "Note",
+    "Project",
+    "ProjectIdea",
+    "ProjectStage",
+    "ProjectTag",
+    "Resource",
+    "Setting",
+    "Task",
+    "WorkflowStage",
+]
